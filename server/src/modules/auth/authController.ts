f43e221b0701909fc11bc28cf1d5875b0ad180bb
Adarsha Gautam <gautamadarsha08@gmail.com>
2026-04-27 17:27:21 +0545

    Improving error message response

--- server/src/modules/auth/authController.ts
+++ server/src/modules/auth/authController.ts
@@ -25,7 +25,7 @@ export const login = async (req: Request, res: Response,next: NextFunction) => {
       httpOnly: true,
       secure: true,
       sameSite: 'strict',
-      maxAge: 24 * 60 * 60 * 1000
+      maxAge: 24 * 60 * 60 * 1000 
     })
 
     return res.status(200).json({ message: 'Login successful', accessToken })
--- server/src/server.ts
+++ server/src/server.ts
@@ -35,7 +35,6 @@ app.use('/member',memberRoute)
 app.use(errorHandler)
 
 
-
 app.listen(5000, () => {
   console.log('Server running on port 5000')
 })
